2e2bee985472925aec2d782b78b035fcba7b4913
Evan Ramer <ramer.16@osu.edu>
2018-02-16 11:43:37 -0500

    Finished patients module and patients unit tests.

--- tests/patients.test.js
+++ tests/patients.test.js
@@ -1,5 +1,30 @@
-const patients = require('../lib/patients.js');
+describe('randomPatient', () => {
+  test('returns a random patient json object', () => {
+    const patients = require('../lib/patients.js');
+    const allPatients = patients.getPatients;
+  
+    expect(allPatients).toContain(patients.randomPatient());
+  });
+});
 
-test('returns random patient', () => {
-  expect(patients.randomPatient().name).toBe("Michelle");
+describe('getPatient', () => {
+  describe('given the valid name Michelle', () => {
+    test('returns the Michelle patient object', () => {
+      const patients = require('../lib/patients.js');  
+    
+      expect(patients.getPatient("Michelle")).toMatchObject({
+        name: "Michelle",
+        image: "/images/profile/patient.jpg",
+        workspaceId: "ea449515-d489-495e-bf26-268803124a8b"
+      });
+    }); 
+  }); 
+
+  describe('given an invalid name', () => {
+    test('returns null', () => {
+      const patients = require('../lib/patients.js');  
+
+      expect(patients.getPatient("asdf")).toBeNull();
+    });
+  }); 
 });
