be79803688c0b4351f7262ca9868c287bedd2f11
Evan Ramer <ramer.16@osu.edu>
2018-03-06 18:51:42 -0500

    Added new images for each patient and displayed properly.

--- tests/patients.test.js
+++ tests/patients.test.js
@@ -14,7 +14,7 @@ describe('getPatient', () => {
     
       expect(patients.getPatient("Michelle")).toMatchObject({
         name: "Michelle",
-        image: "/images/profile/generic-patient.png",
+        image: "/images/profile/amy.png",
         workspaceId: "ea449515-d489-495e-bf26-268803124a8b"
       });
     }); 
